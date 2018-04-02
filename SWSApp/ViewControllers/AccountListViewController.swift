434fde248373e2f7719e882be2b3f704fcaa4438
Shilpa Kulkarni <shilpa.a.kulkarni@accenture.com>
2018-04-02 20:02:25 +0530

    1047, 1055, 1032-AccountList UI

---
+++ SWSApp/ViewControllers/AccountListViewController.swift
@@ -0,0 +1,109 @@
+//
+//  AccountListViewController.swift
+//  SWSApp
+//
+//  Created by shilpa.a.kulkarni on 29/03/18.
+//  Copyright © 2018 maria.min-hui.yu. All rights reserved.
+//
+
+import UIKit
+
+/// <#Description#>
+class AccountsListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var accountListTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 200.0
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell:AccountRowCell = tableView.dequeueReusableCell(withIdentifier: "accountRowCellID", for: indexPath) as! AccountRowCell
+        
+        return cell
+        
+    }
+    
+    // custom header for the section
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?
+    {
+        let headerCell:SortingCell = tableView.dequeueReusableCell(withIdentifier: "sortingCellID") as! SortingCell
+        return headerCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 80.0;
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat{
+        return 0;
+    }
+    
+    
+    /// <#Description#>
+    ///
+    /// - Parameter animated: <#animated description#>
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Accounts List VC will appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("Accounts List VC will disappear")
+    }
+    
+    // # MARK: Account List Sorting related
+    @IBAction func sortAccountListByAccountName(_ sender: Any)
+    {
+        print("sortAccountListByAccountName")
+    }
+    
+    @IBAction func sortAccountListByActionItems(_ sender: Any)
+    {
+        print("sortAccountListByActionItems")
+    }
+    
+    @IBAction func sortAccountListByNetSales(_ sender: Any)
+    {
+        print("sortAccountListByActionItems")
+    }
+    
+    @IBAction func sortAccountListByBalance(_ sender: Any)
+    {
+        print("sortAccountListByBalance")
+    }
+    
+    @IBAction func sortAccountListByNextDelivery(_ sender: Any)
+    {
+        print("sortAccountListByNextDelivery")
+    }
+}
+
+// sorting table view cell
+class SortingCell: UITableViewCell
+{
+    
+    @IBOutlet weak var SortByNextDeliveryView: UIView!
+    @IBOutlet weak var SortByBalanceView: UIView!
+    @IBOutlet weak var SortByNetSalesView: UIView!
+    @IBOutlet weak var SortByActionItemsView: UIView!
+    @IBOutlet weak var SortByAccountNameView: UIView!
+}
+
+// account row cell
+class AccountRowCell: UITableViewCell
+{
+    
+}
+
---
+++ SWSApp/ViewControllers/MoreViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MoreViewController.swift
+//  SWSApp
+//
+//  Created by shilpa.a.kulkarni on 02/04/18.
+//  Copyright © 2018 maria.min-hui.yu. All rights reserved.
+//
+
+import UIKit
+
+class MoreViewController: UIViewController {
+    
+    @IBOutlet weak var moreLabel: UILabel!
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("More VC will appear")
+        self.moreLabel.text = "More"
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("More VC will disappear")
+    }
+}

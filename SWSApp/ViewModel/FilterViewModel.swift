943b5833b9f74997ce0cb42b1ef78a2f7260ea34
maria.min-hui.yu <maria.min-hui.yu@accenture.com>
2018-04-03 00:11:21 -0400

    Add ViewModel files; move dispatcher to Dispatcher group.

---
+++ SWSApp/ViewModel/FilterViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModel.swift
+//  ExpandableHeaders
+//
+//  Created by Krishna, Kamya on 3/29/18.
+//  Copyright © 2018 Anexinet. All rights reserved.
+//
+
+import Foundation
+
+class FilterViewModel {
+    var pastDue: String = ""
+    var status: String = ""
+    var premise: String = ""
+    var locations: String = ""
+    var channel: String = ""
+    var subChannel: String = ""
+    var licenseType: String = ""
+    var city: String = ""
+}

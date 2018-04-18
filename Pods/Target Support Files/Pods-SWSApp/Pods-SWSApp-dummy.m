3e8fd8b9a6a35f506c5026fe40c4d792dc4682e1
Manohar Kale, Saagar <saagar.manohar.kale@accenture.com>
2018-04-18 13:58:21 +0530

    Reset to upload Release 1.0

--- Pods/Target Support Files/Pods-SWSApp/Pods-SWSApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SWSApp : NSObject
-@end
-@implementation PodsDummy_Pods_SWSApp
-@end
--- Pods/Target Support Files/SalesforceSwiftSDK/SalesforceSwiftSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SalesforceSwiftSDK : NSObject
-@end
-@implementation PodsDummy_SalesforceSwiftSDK
-@end

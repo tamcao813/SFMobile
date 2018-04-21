8fc892344a3c6dfa9ea58c788188dc71d905a87c
Thillaiganesh, C <c.thillaiganesh@accenture.com>
2018-04-21 16:08:40 +0530

    Contact Query : Storyboard, ViewController, UI created.

---
+++ SWSApp/ViewControllers/ContactMenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContactMenuViewController.swift
+//  SWSApp
+//
+//  Created by Thillaiganesh, C. on 21/04/18.
+//  Copyright © 2018 maria.min-hui.yu. All rights reserved.
+//
+
+import UIKit
+
+class ContactMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

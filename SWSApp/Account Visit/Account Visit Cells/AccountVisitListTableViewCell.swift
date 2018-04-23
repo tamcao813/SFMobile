a4cd3e4399bcf1f19215615a8cbe479eb878d415
Kamya <kamya.krishna@accenture.com>
2018-04-23 19:13:35 +0530

    Added Account Visit Summary Screen

---
+++ SWSApp/Account Visit/Account Visit Cells/AccountVisitListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AccountVisitListTableViewCell.swift
+//  SWSApp
+//
+//  Created by Krishna, Kamya on 4/23/18.
+//  Copyright © 2018 maria.min-hui.yu. All rights reserved.
+//
+
+import UIKit
+
+class AccountVisitListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

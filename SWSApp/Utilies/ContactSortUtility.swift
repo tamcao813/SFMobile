17fa579e5ea023806cc676cc508981fe390ed488
Thillaiganesh, C <c.thillaiganesh@accenture.com>
2018-04-24 17:54:18 +0530

    Implemented : Contact Query in Contact List for Contact's on route / Account ID

--- SWSApp/Utilies/ContactSortUtility.swift
+++ SWSApp/Utilies/ContactSortUtility.swift
@@ -31,12 +31,82 @@ class ContactSortUtility {
     
     static func filterContactByAppliedFilter(contactListToBeSorted : [Contact], searchBarText:String?)-> [Contact]{
         
-        var filteredContactArray = [Contact]()
+        var filteredContactArray = contactListToBeSorted
         var filteredByReturnArray = [Contact]()
-        var enteredAnyFilterCase = false
         var enteredAnyFilterCaseReturn = false
         
+        // filter by contact association
+        (enteredAnyFilterCaseReturn, filteredByReturnArray) = filterContactByFilterByAssociation(contactListToBeSorted: filteredContactArray)
+        if enteredAnyFilterCaseReturn {
+            filteredContactArray = filteredByReturnArray
+        }
+
         // filter by role
+        (enteredAnyFilterCaseReturn, filteredByReturnArray) = filterContactByFilterByRoles(contactListToBeSorted: filteredContactArray)
+        if enteredAnyFilterCaseReturn {
+            filteredContactArray = filteredByReturnArray
+        }
+        
+        // filter by Buying Power
+        (enteredAnyFilterCaseReturn, filteredByReturnArray) = filterContactByFilterByBuyingPower(contactListToBeSorted: filteredContactArray)
+        if enteredAnyFilterCaseReturn {
+            filteredContactArray = filteredByReturnArray
+        }
+        
+        // now search filtered list by search text
+        if(searchBarText != "")
+        {
+            filteredContactArray = searchContactBySearchBarQuery(contactsForLoggedUser: filteredContactArray, searchText: searchBarText!)
+        }
+
+        return filteredContactArray
+    }
+    
+    static func filterContactByFilterByAssociation(contactListToBeSorted : [Contact])-> (Bool, [Contact]){
+        
+        var filteredContactArray = [Contact]()
+        var enteredAnyFilterCase = false
+
+        if ContactFilterMenuModel.allContacts == "YES" {
+            enteredAnyFilterCase = true
+            filteredContactArray = contactListToBeSorted
+        }
+        else if ContactFilterMenuModel.contactsOnMyRoute == "YES" {
+            
+            enteredAnyFilterCase = true
+            let accountViewModel = AccountsViewModel()
+            print(accountViewModel.accountsForLoggedUser)
+            if accountViewModel.accountsForLoggedUser.count > 0 {
+                
+                var filteredAccountContactArray = [Contact]()
+                for account in accountViewModel.accountsForLoggedUser {
+                    filteredAccountContactArray += contactListToBeSorted.filter( { return account.account_Id == $0.accountId } )
+                }
+
+                if filteredContactArray.count > 0 {
+                    let filteredNoDuplicateContactArray = filteredAccountContactArray.reduce([]) { (r, p) -> [Contact] in
+                        var r2 = r
+                        if !r.contains (where: { $0.contactId == p.contactId }) {
+                            r2.append(p)
+                        }
+                        return r2
+                    }
+                    
+                    filteredContactArray = filteredNoDuplicateContactArray
+                }
+
+            }
+            
+        }
+        
+        return (enteredAnyFilterCase, filteredContactArray)
+    }
+
+    static func filterContactByFilterByRoles(contactListToBeSorted : [Contact])-> (Bool, [Contact]){
+        
+        var filteredContactArray = [Contact]()
+        var enteredAnyFilterCase = false
+        
         if ContactFilterMenuModel.allRole == "YES"{
             enteredAnyFilterCase = true
             filteredContactArray = contactListToBeSorted
@@ -45,32 +115,8 @@ class ContactSortUtility {
             enteredAnyFilterCase = true
             filteredContactArray = contactListToBeSorted.filter( { return ContactFilterMenuModel.functionRoles.contains($0.functionRole) } )
         }
-        
-        // filter by Buying Power
-        if(enteredAnyFilterCase == false)
-        {
-            (enteredAnyFilterCaseReturn, filteredByReturnArray) = filterContactByFilterByBuyingPower(contactListToBeSorted: contactListToBeSorted)
-        }
-        else
-        {
-            (enteredAnyFilterCaseReturn, filteredByReturnArray) = filterContactByFilterByBuyingPower(contactListToBeSorted: filteredContactArray)
-        }
-        if enteredAnyFilterCaseReturn {
-            enteredAnyFilterCase = true
-            filteredContactArray = filteredByReturnArray
-        }
-        
-        // now search filtered list by search text
-        if(searchBarText != "" && enteredAnyFilterCase == true) // user filtered list fot search
-        {
-            filteredContactArray = searchContactBySearchBarQuery(contactsForLoggedUser: filteredContactArray, searchText: searchBarText!)
-        }
-        else if(searchBarText != "" && enteredAnyFilterCase == false) // use main list for search
-        {
-            filteredContactArray = searchContactBySearchBarQuery(contactsForLoggedUser: contactListToBeSorted, searchText: searchBarText!)
-        }
 
-        return filteredContactArray
+        return (enteredAnyFilterCase, filteredContactArray)
     }
     
     static func filterContactByFilterByBuyingPower(contactListToBeSorted : [Contact])-> (Bool, [Contact]){
@@ -87,28 +133,27 @@ class ContactSortUtility {
         }
         else {
             // filter by Buying Power
+            var filteredBuyingPowerContactArray = [Contact]()
+            var filteredNoBuyingPowerContactArray = [Contact]()
+
             if ContactFilterMenuModel.buyingPower == "YES"{
-                if(enteredAnyFilterCase == false)
-                {
-                    enteredAnyFilterCase = true
-                    filteredContactArray = contactListToBeSorted.filter( { return $0.buyerFlag == "1" } )
-                }
-                else {
-                    filteredContactArray = filteredContactArray.filter( { return $0.buyerFlag == "1" } )
-                }
+                filteredBuyingPowerContactArray = contactListToBeSorted.filter( { return $0.buyerFlag == "1" } )
             }
             
             // filter by NO Buying Power
             if ContactFilterMenuModel.nobuyingPower == "YES"{
-                if(enteredAnyFilterCase == false)
-                {
-                    enteredAnyFilterCase = true
-                    filteredContactArray = contactListToBeSorted.filter( { return $0.buyerFlag == "0" } )
-                }
-                else {
-                    filteredContactArray = filteredContactArray.filter( { return $0.buyerFlag == "0" } )
-                }
+                filteredNoBuyingPowerContactArray = contactListToBeSorted.filter( { return $0.buyerFlag == "0" } )
             }
+            
+            if filteredBuyingPowerContactArray.count > 0 {
+                enteredAnyFilterCase = true
+                filteredContactArray = filteredBuyingPowerContactArray
+            }
+            if filteredNoBuyingPowerContactArray.count > 0 {
+                enteredAnyFilterCase = true
+                filteredContactArray += filteredNoBuyingPowerContactArray
+            }
+
         }
         
         return (enteredAnyFilterCase, filteredContactArray)

fcc76d28fdab3e1800c75373631621eb37fa2a98
r.a.jantakal <r.a.jantakal@accenture.com>
2018-04-25 12:02:27 +0530

    Account Strategy Screen implementation

--- SWSApp/ViewControllers/TableViewCells/EditAccountStrategyCollectionReusableView.swift
+++ SWSApp/ViewControllers/TableViewCells/EditAccountStrategyCollectionReusableView.swift
@@ -2,22 +2,29 @@
 //  AccountStrategyCollectionReusableView.swift
 //  SWSApp
 //
-//  Created by r.a.jantakal on 24/04/18.
+//  Created by r.a.jantakal on 23/04/18.
 //  Copyright © 2018 maria.min-hui.yu. All rights reserved.
 //
 
 import UIKit
 
-class AccountStrategyCollectionReusableView: UICollectionReusableView {
+class EditAccountStrategyCollectionReusableView: UICollectionReusableView {
     
     @IBOutlet weak var headerLabel : UILabel?
     @IBOutlet weak var questionLabel : UILabel?
-    
+    @IBOutlet weak var selectionLabel : UILabel?
     
     //Used to present the header data
     func displayHeaderViewData(data : NSMutableArray , indexPath : IndexPath){
+        self.isHidden = false
         let headerQuestion = data[indexPath.section] as! NSDictionary
         self.headerLabel?.text = (headerQuestion["headerText"] as! String)
         self.questionLabel?.text = (headerQuestion["question"] as! String)
+        
+        if (headerQuestion["selectionType"] as! String) == "1"{
+            self.selectionLabel?.text = "Single Select"
+        }else{
+            self.selectionLabel?.text = "Multi Select"
+        }
     }
 }

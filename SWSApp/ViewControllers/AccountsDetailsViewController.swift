30b9c838e955d7129e80185ed4dd5e011eddc925
shubham.e.shukla <shubham.e.shukla@accenture.com>
2018-04-27 15:12:06 +0530

    Sonar Bug Fixed In AccountDetailTabViewController Sonar Bug Fixed In AccountDetailsViewController

--- SWSApp/ViewControllers/AccountsDetailsViewController.swift
+++ SWSApp/ViewControllers/AccountsDetailsViewController.swift
@@ -125,7 +125,6 @@ class AccountDetailsViewController : UIViewController{
         }
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Account details Screen is loaded")
@@ -137,7 +136,33 @@ class AccountDetailsViewController : UIViewController{
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        self.setupDetailsScreenUI()
+        self.setupPastDueUI()
+        self.setupAccountHealthGrade()
+        self.setupPercentageValue()
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        
+    }
+   
+    
+    func setupDetailsScreenUI(){
+        
         lblAccountTitle?.text = accountDetailForLoggedInUser?.accountName
+        lblActionItem?.text = String(describing: accountDetailForLoggedInUser!.actionItem)
+        lblPastDue?.text = CurrencyFormatter.convertToCurrencyFormat(amountToConvert: (accountDetailForLoggedInUser?.pastDueAmountDouble)!) //String(format: "$%.2f",(accountDetailForLoggedInUser?.pastDueAmountDouble)!)
+        lblCYR12Sales?.text = CurrencyFormatter.convertToCurrencyFormat(amountToConvert: (accountDetailForLoggedInUser?.totalCYR12NetSales)!)
+        lblLicenseStatus?.text = accountDetailForLoggedInUser?.licenseStatus
+        lblPhoneNumber?.text = accountDetailForLoggedInUser?.phone
         
         if let acc = accountDetailForLoggedInUser{
             
@@ -158,51 +183,38 @@ class AccountDetailsViewController : UIViewController{
             }
             lblAddress1?.text = fullAddress
         }
-        
-        //let pastDue : Double = Double((accountDetailForLoggedInUser?.pastDueAmount)!)
+    }
+    
+    func setupPastDueUI(){
         
         if accountDetailForLoggedInUser!.pastDueAmountDouble <= 0.0 {
-            
             imgStatus?.isHidden = true
-        }
-        else {
-            
+        }else {
             imgStatus?.isHidden = false
         }
-        
+    }
+    
+    func setupAccountHealthGrade(){
         if accountDetailForLoggedInUser?.acctHealthGrade == "A"{
-            
             centerLabel?.text = accountDetailForLoggedInUser?.acctHealthGrade
             centerLabel?.layer.backgroundColor = UIColor(named: "Good")?.cgColor//green.cgColor
-            
         }else if accountDetailForLoggedInUser?.acctHealthGrade == "B"{
-            
             centerLabel?.text = accountDetailForLoggedInUser?.acctHealthGrade
             centerLabel?.layer.backgroundColor = UIColor(named: "Medium Alert")?.cgColor//yellow.cgColor
-            
         }else if accountDetailForLoggedInUser?.acctHealthGrade == "C"{
-            
             centerLabel?.text = accountDetailForLoggedInUser?.acctHealthGrade
             centerLabel?.layer.backgroundColor = UIColor.orange.cgColor
             
         }else if accountDetailForLoggedInUser?.acctHealthGrade == "D"{
-            
             centerLabel?.text = accountDetailForLoggedInUser?.acctHealthGrade
             centerLabel?.layer.backgroundColor = UIColor(named: "Bad")?.cgColor//.red.cgColor
-            
-        }
-        else {
-            
+        } else {
             centerLabel?.layer.backgroundColor = UIColor.clear.cgColor
-            
         }
-        
-        
-        lblActionItem?.text = String(describing: accountDetailForLoggedInUser!.actionItem)
-        lblPastDue?.text = CurrencyFormatter.convertToCurrencyFormat(amountToConvert: (accountDetailForLoggedInUser?.pastDueAmountDouble)!) //String(format: "$%.2f",(accountDetailForLoggedInUser?.pastDueAmountDouble)!)
-        lblCYR12Sales?.text = CurrencyFormatter.convertToCurrencyFormat(amountToConvert: (accountDetailForLoggedInUser?.totalCYR12NetSales)!)
-        lblLicenseStatus?.text = accountDetailForLoggedInUser?.licenseStatus
-        lblPhoneNumber?.text = accountDetailForLoggedInUser?.phone
+    }
+    
+    
+    func setupPercentageValue(){
         
         let percLastYearR12DivideBy100:Double = ((accountDetailForLoggedInUser?.percentageLastYearR12NetSales)!as NSString).doubleValue / 100
         let percentYearR12Double:Double =  ((accountDetailForLoggedInUser?.percentageLastYearR12NetSales)!as NSString).doubleValue
@@ -213,47 +225,22 @@ class AccountDetailsViewController : UIViewController{
         
         let titleForButton = String(format: "%.02f",percentYearR12Double) + "%"
         
-        
-        
         print("the button text is \(titleForButton)")
         
         if percLastYearR12DivideBy100 < 0.80 {
-            
-            
             btnPercentage?.setTitle(titleForButton, for: .normal)
             btnPercentage?.backgroundColor = UIColor(named: "Bad")
-            
         }else if percLastYearR12DivideBy100 >= 0.80 && percLastYearR12DivideBy100 <= 0.99 {
-            
             btnPercentage?.setTitle(titleForButton, for: .normal)
             btnPercentage?.backgroundColor = UIColor(named: "Medium Alert")
-            
         }
         else if percLastYearR12DivideBy100 > 0.99 {
-            
             btnPercentage?.setTitle(titleForButton, for: .normal)
             btnPercentage?.backgroundColor = UIColor(named: "Good")
-            
-            
         }
-        
-        
-        
-        
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        
-    }
-    
+   
     @IBAction func itemsClicked(sender : UIButton){
         
         containerView?.isHidden = true

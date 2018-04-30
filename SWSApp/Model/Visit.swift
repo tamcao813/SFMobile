76323a98174aa9c8d91187aa37c05b03da6815d2
r.a.jantakal <r.a.jantakal@accenture.com>
2018-04-30 15:01:41 +0530

    Visit Model File

---
+++ SWSApp/Model/Visit.swift
@@ -0,0 +1,16 @@
+//
+//  Visit.swift
+//  SWSApp
+//
+//  Created by r.a.jantakal on 30/04/18.
+//  Copyright © 2018 maria.min-hui.yu. All rights reserved.
+//
+
+import Foundation
+
+class Visit{
+    
+    static let VisitsFields: [String] = ["Id","LastModifiedDate","Name","OwnerId","SGWS_Account__c","SGWS_Description__c"]
+    
+    
+}

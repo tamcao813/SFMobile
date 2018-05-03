9a723d44486c664543dffff3101892f39c77c53e
Thillaiganesh, C <c.thillaiganesh@accenture.com>
2018-05-03 12:14:17 +0530

    Implemented in Contact Details To navigate back to the Account record, the user can click the Account from the Linked Accounts list once the Account has been added to the Contact.

--- SWSApp/ViewControllers/ParentViewController.swift
+++ SWSApp/ViewControllers/ParentViewController.swift
@@ -119,8 +119,9 @@ class ParentViewController: UIViewController, XMSegmentedControlDelegate{
         } catch {
             print("Unable to start notifier")
         }
+        NotificationCenter.default.addObserver(self, selector: #selector(self.showAllAccounts), name: NSNotification.Name("showAllAccounts"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.showAllContacts), name: NSNotification.Name("showAllContacts"), object: nil)
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(self.loadMoreScreens), name: NSNotification.Name("loadMoreScreens"), object: nil)
         
     }
@@ -135,13 +136,19 @@ class ParentViewController: UIViewController, XMSegmentedControlDelegate{
         // Dispose of any resources that can be recreated.
     }
     
+    @objc func showAllAccounts(notification: NSNotification){
+        topMenuBar?.selectedSegment = 1
+        _ = displayCurrentTab(1)
+        
+    }
+    
     @objc func showAllContacts(notification: NSNotification){
         if notification.object != nil{
             ContactsGlobal.accountId = notification.object as! String
         }
         print(notification.object)
         topMenuBar?.selectedSegment = 2
-        displayCurrentTab(2)
+        _ = displayCurrentTab(2)
         
     }
     

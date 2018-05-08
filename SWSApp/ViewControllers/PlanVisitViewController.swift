45d29430a77230824148c6dc5686bc4fa8d5b1e5
Thillaiganesh, C <c.thillaiganesh@accenture.com>
2018-05-08 12:19:13 +0530

    Fix : plan visit autolayout

--- SWSApp/ViewControllers/PlanVisitViewController.swift
+++ SWSApp/ViewControllers/PlanVisitViewController.swift
@@ -49,6 +49,8 @@ class PlanVisitViewController: UIViewController, CloseAccountViewDelegate {
     @IBOutlet var searchContactTxt: DesignableUITextField!
     @IBOutlet var schedulerComponentView: SchedulerComponent!
     
+    @IBOutlet weak var bottomViewSpacing: NSLayoutConstraint!
+    
     // MARK:- View Life Cycle
     
     override func viewDidLoad() {
@@ -72,6 +74,8 @@ class PlanVisitViewController: UIViewController, CloseAccountViewDelegate {
         self.scrollView.addSubview(self.associatedContactTableView)
         self.associatedContactTableView.isHidden = true
         self.textFieldTag = kSelectedContactTag
+        
+        bottomViewSpacing.constant = 30
         
         //self.createNewVisit()
         
@@ -118,7 +122,8 @@ class PlanVisitViewController: UIViewController, CloseAccountViewDelegate {
         // Adjust View
         
         self.associatedContactTableView.isHidden = true
-        
+        bottomViewSpacing.constant = 30
+
         /* TBD
         self.bottomView.frame = CGRect(x: 0, y: self.searchContactTxt.frame.origin.y +  self.searchContactTxt.frame.size.height + 20, width: self.bottomView.frame.size.width, height: self.bottomView.frame.size.height)
         
@@ -145,12 +150,17 @@ class PlanVisitViewController: UIViewController, CloseAccountViewDelegate {
                 self.associatedContactTableView.reloadData()
                 if (self.associatedSelectedContact.count > 0) {
                     self.associatedContactTableView.frame = CGRect(x: self.searchContactTxt.frame.origin.x, y: self.searchContactTxt.frame.origin.y + self.searchContactTxt.frame.size.height + 40, width: self.searchContactTxt.frame.size.width, height: CGFloat(102 * self.associatedSelectedContact.count))
+                    
+                    bottomViewSpacing.constant = 30 + associatedContactTableView.frame.height + 20
+
                     /* TBD
                     self.bottomView.frame = CGRect(x: 0, y: self.associatedContactTableView.frame.origin.y +  self.associatedContactTableView.frame.size.height + 20, width: self.bottomView.frame.size.width, height: self.bottomView.frame.size.height)
                     
                     self.scrollView.contentSize = CGSize(width: self.scrollView.frame.size.width, height: self.scrollView.frame.size.height + CGFloat(102 * self.associatedSelectedContact.count) + 40)*/
                 } else {
                     self.associatedContactTableView.isHidden = true
+                    bottomViewSpacing.constant = 30
+
                     /* TBD
                     self.bottomView.frame = CGRect(x: 0, y: self.searchContactTxt.frame.origin.y +  self.searchContactTxt.frame.size.height + 20, width: self.bottomView.frame.size.width, height: self.bottomView.frame.size.height)
                     
@@ -292,6 +302,8 @@ class PlanVisitViewController: UIViewController, CloseAccountViewDelegate {
             
             self.associatedContactTableView.frame = CGRect(x: self.searchContactTxt.frame.origin.x, y: self.searchContactTxt.frame.origin.y + self.searchContactTxt.frame.size.height + 40, width: self.searchContactTxt.frame.size.width, height: CGFloat(102 * self.associatedSelectedContact.count))
             
+            bottomViewSpacing.constant = 30 + associatedContactTableView.frame.height + 20
+
             /* TBD
             self.bottomView.frame = CGRect(x: 0, y: self.associatedContactTableView.frame.origin.y +  self.associatedContactTableView.frame.size.height + 20, width: self.bottomView.frame.size.width, height: self.bottomView.frame.size.height)
             
@@ -568,6 +580,8 @@ extension PlanVisitViewController : UITableViewDelegate {
                 self.associatedContactTableView.reloadData()
                 
                 self.associatedContactTableView.frame = CGRect(x: self.searchContactTxt.frame.origin.x, y: self.searchContactTxt.frame.origin.y + self.searchContactTxt.frame.size.height + 40, width: self.searchContactTxt.frame.size.width, height: CGFloat(102 * self.associatedSelectedContact.count))
+                self.bottomViewSpacing.constant = 30 + self.associatedContactTableView.frame.height + 20
+
                 /* TBD
                 self.bottomView.frame = CGRect(x: 0, y: self.associatedContactTableView.frame.origin.y +  self.associatedContactTableView.frame.size.height + 20, width: self.bottomView.frame.size.width, height: self.bottomView.frame.size.height)
                 

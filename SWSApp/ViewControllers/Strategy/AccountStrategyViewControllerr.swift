bb10a1d960cc2f6533d4f32ef06a0fcbdd000601
r.a.jantakal <r.a.jantakal@accenture.com>
2018-05-09 16:52:53 +0530

    Crash Fix when load from During Visit to edit Strategy

--- SWSApp/ViewControllers/Strategy/AccountStrategyViewControllerr.swift
+++ SWSApp/ViewControllers/Strategy/AccountStrategyViewControllerr.swift
@@ -268,6 +268,7 @@ class AccountStrategyViewController : UIViewController{
     
     //MARK:- Button Actions
     @IBAction func editButtonClicked(sender : UIButton){
+        LoadEditStrategyFromDuringVisit.editStrategy = "0"
         performSegue(withIdentifier: "editStrategySegue", sender: nil)
         
     }

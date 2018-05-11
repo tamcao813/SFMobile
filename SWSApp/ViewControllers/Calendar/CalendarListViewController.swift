6a9562b1f75eb024bcd14147d701338a46c849e0
Thillaiganesh, C <c.thillaiganesh@accenture.com>
2018-05-11 10:46:32 +0530

    Calendar View : - Added calendar storyboard and viewcontrollers

---
+++ SWSApp/ViewControllers/Calendar/CalendarListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CalendarListViewController.swift
+//  SWSApp
+//
+//  Created by Thillaiganesh, C. on 11/05/18.
+//  Copyright © 2018 maria.min-hui.yu. All rights reserved.
+//
+
+import UIKit
+
+class CalendarListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}

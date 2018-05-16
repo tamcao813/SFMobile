94a1fe9bf56d0004f37d848f133c246601d1f75e
Thillaiganesh, C <c.thillaiganesh@accenture.com>
2018-05-16 18:48:58 +0530

    Added UIButtonTitleUtil.displayAttributedTitle

--- SWSApp/ViewControllers/Calendar/CalendarListViewController.swift
+++ SWSApp/ViewControllers/Calendar/CalendarListViewController.swift
@@ -103,27 +103,7 @@ class CalendarListViewController: UIViewController {
 
     // MARK: - Addnew Button Text
     func setupAddNewButtonText() {
-        let smallString = "Add New "
-        let bigString = "+"
-        let fullString = "\(smallString) \(bigString)"
-        let attrString = NSMutableAttributedString(string: fullString,
-                                                         attributes: [NSAttributedStringKey.foregroundColor : UIColor.white])
-
-        let smallStringRange = NSRange(location: 0, length: smallString.count)
-        let bigStringRange = NSRange(location: smallStringRange.length+1, length: bigString.count)
-        
-        let smallStringFontSize: CGFloat = 17
-        let bigStringFontSize: CGFloat = 30
-        
-        attrString.beginEditing()
-        
-        attrString.addAttribute(.font, value: UIFont(name:"Ubuntu", size: smallStringFontSize)!, range: smallStringRange)
-        attrString.addAttribute(.font, value: UIFont(name:"Ubuntu", size: bigStringFontSize)!, range: bigStringRange)
-        attrString.addAttribute(.baselineOffset, value: (bigStringFontSize - smallStringFontSize) / 4, range: smallStringRange)
-        
-        attrString.endEditing()
-        
-        addNewButton.setAttributedTitle(attrString, for: .normal)
+        UIButtonTitleUtil.displayAttributedTitle(addNewButton, smallString: "Add New ", bigString: "+")
     }
     
     // MARK: - DropDown Addnew

fef7b08a5b2d27532f588ee4a37bf92a4d3d40d0
shubham.e.shukla <shubham.e.shukla@accenture.com>
2018-05-18 12:42:54 +0530

    Comment print

--- SWSApp/Utilies/GlobalConstants.swift
+++ SWSApp/Utilies/GlobalConstants.swift
@@ -27,7 +27,7 @@ class GlobalConstants
 }
 
 public func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-    let output = items.map { "\($0)" }.joined(separator: separator)
-    Swift.print(output, terminator: terminator)
+    //let output = items.map { "\($0)" }.joined(separator: separator)
+    //Swift.print(output, terminator: terminator)
 }
 

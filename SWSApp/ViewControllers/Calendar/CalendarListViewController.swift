ebed87bc712f5f9090a27d25e4e44d18e8fd2309
soumin.nikhra <soumin.nikhra@accenture.com>
2018-05-21 14:17:04 +0530

    selectEvent check is commented

--- SWSApp/ViewControllers/Calendar/CalendarListViewController.swift
+++ SWSApp/ViewControllers/Calendar/CalendarListViewController.swift
@@ -261,7 +261,7 @@ extension CalendarListViewController: WRWeekViewDelegate {
     func selectEvent(_ event: WREvent) {
         print("selectEvent: WREvent.Id: \(event.Id) : WREvent.title: \(event.title) : WREvent.type: \(event.type)")
         
-        if event.type == "visit" {
+//        if event.type == "visit" {
             PlanVisitManager.sharedInstance.visit = WorkOrderUserObject(for: "") // Todo read visit object from VisitViewModel
             PlanVisitManager.sharedInstance.visit?.Id = event.Id
             
@@ -272,7 +272,7 @@ extension CalendarListViewController: WRWeekViewDelegate {
             DispatchQueue.main.async {
                 self.present(accountVisitsVC!, animated: true, completion: nil)
             }
-        }
+//        }
 
     }
 }

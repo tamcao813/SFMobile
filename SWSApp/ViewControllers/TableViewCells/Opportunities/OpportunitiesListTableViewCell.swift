cd8d6e11403351670e24b4c362cd7e9c9a46175a
Thillaiganesh, C <c.thillaiganesh@accenture.com>
2018-06-05 16:15:11 +0530

    OpportunitiesTab: - ListView UI

---
+++ SWSApp/ViewControllers/TableViewCells/Opportunities/OpportunitiesListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OpportunitiesListTableViewCell.swift
+//  SWSApp
+//
+//  Created by Thillaiganesh, C. on 05/06/18.
+//  Copyright © 2018 maria.min-hui.yu. All rights reserved.
+//
+
+import UIKit
+
+class OpportunitiesListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

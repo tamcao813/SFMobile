2cbe05b8535b627642447952ddf274f94e361872
r.a.jantakal <r.a.jantakal@accenture.com>
2018-06-06 02:02:00 +0530

    Removed Unwanted Lines

--- SWSApp/ViewControllers/HomeViewController.swift
+++ SWSApp/ViewControllers/HomeViewController.swift
@@ -35,9 +35,7 @@ class HomeViewController: UIViewController {
         print("Home VC will appear")
         
         //        registerSyncLogSoup()
-        
-        
-        
+
         //for soup testing
         //        let SmartStoreViewController = SFSmartStoreInspectorViewController.init(store:  SFSmartStore.sharedStore(withName: StoreDispatcher.SFADB) as! SFSmartStore)
         //        present(SmartStoreViewController, animated: true, completion: nil)

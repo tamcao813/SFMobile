681d90a32c59f958c940f76aeb8324b23acf6749
Thillaiganesh, C <c.thillaiganesh@accenture.com>
2018-06-13 17:50:23 +0530

    Opportunity: - Updated query with ‘StageName’

--- SWSApp/Model/OpportunityWorkorder.swift
+++ SWSApp/Model/OpportunityWorkorder.swift
@@ -11,6 +11,7 @@ import UIKit
 class OpportunityWorkorder {
 
     static let opportunityWorkorderFields: [String] = [ "Id", "SGWS_Opportunity__c", "SGWS_Work_Order__c", "SGWS_Outcome__c" ]
+    static let opportunityWorkorderSyncUpFields: [String] = [ "SGWS_Opportunity__c", "SGWS_Work_Order__c", "SGWS_Outcome__c" ]
 
     var id: String
     var opportunityId: String

c322f2f45e7840140f5becbc33211a73bb23bd07
manu.a.gupta <manu.a.gupta@accenture.com>
2018-06-15 14:28:20 +0530

    Read/ Unread marking for notification is not proper when more than 10 notification, last notification in the list not able to mark as Read/ Unread

--- SWSApp/ViewControllers/ParentViewController.swift
+++ SWSApp/ViewControllers/ParentViewController.swift
@@ -219,7 +219,6 @@ class ParentViewController: UIViewController, XMSegmentedControlDelegate{
         if notification.object != nil{
             ContactsGlobal.accountId = notification.object as! String
         }
-        //        print(notification.object!)
         topMenuBar?.selectedSegment = 2
         _ = displayCurrentTab(2)
         

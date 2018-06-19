e1939377588e0e9d4b61a55988676fa4b23cc636
Thillaiganesh, C <c.thillaiganesh@accenture.com>
2018-06-19 18:08:59 +0530

    Oppurtunity: RESync

--- SWSApp/ViewModel/OpportunityViewModel.swift
+++ SWSApp/ViewModel/OpportunityViewModel.swift
@@ -35,6 +35,11 @@ class OpportunityViewModel {
                 print("syncOpportunitysWithServer: Sync up failed")
             }
             
+            StoreDispatcher.shared.syncDownOpportunity() { _ in
+                let _ = self.globalOpportunityReload()
+                completion(nil)
+            }
+            /*
             StoreDispatcher.shared.reSyncOpportunity { error in
                 if error != nil {
                     print(error?.localizedDescription ?? "error")
@@ -44,7 +49,7 @@ class OpportunityViewModel {
                 else {
                     completion(nil)
                 }
-            }
+            }*/
         })
     }
 

195a3f1b083105603a160cfbfb0e8f400fa053f8
chandana <chandana.a.reddy@accenture.com>
2018-06-26 18:02:23 +0530

    Source type resolved

--- SWSApp/ViewControllers/Account Visit/AccountsSummaryOpportunityCell.swift
+++ SWSApp/ViewControllers/Account Visit/AccountsSummaryOpportunityCell.swift
@@ -58,7 +58,7 @@ class AccountsSummaryOpportunityCell: UITableViewCell,UITableViewDataSource,UITa
             outcomeValue = thisOppur[0].outcome
         }
         switch currentOpportunity.source {
-        case "What's Hot","Top Seller":
+        case "What's Hot","Top Sellers":
             let cell = tableView.dequeueReusableCell(withIdentifier: "TopSellerCell", for: indexPath) as! AccountsSourceTopSellerTableViewCell
             
             cell.productNameLabel.text = currentOpportunity.productName

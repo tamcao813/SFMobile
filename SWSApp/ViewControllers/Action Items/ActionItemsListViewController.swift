07c9aa0abc6da545584df87cfd8f8bc6009043e5
Jagadeeshwar Reddy <j.kannayyagari@accenture.com>
2018-07-26 15:32:26 +0530

    crash Fixed in Crash issue with during visit flow on click of View Account Notifications

--- SWSApp/ViewControllers/Action Items/ActionItemsListViewController.swift
+++ SWSApp/ViewControllers/Action Items/ActionItemsListViewController.swift
@@ -86,7 +86,6 @@ class ActionItemsListViewController: UIViewController {
                 actionItemsArray = actionItemsArray.filter( { return $0.ownerId == appDelegate.currentSelectedUserId } )
             }
         }else if FilterMenuModel.isFromAccountVisitSummary == "YES"{
-           // actionItemsArray = [ActionItem]()
             let actionItemsArrayLocal = AccountsActionItemViewModel().actionItemFourMonthsDescSorted()
             for actionItem in actionItemsArrayLocal {
                 if actionItem.accountId == (AccountObject.account?.account_Id)! {
@@ -190,7 +189,11 @@ class ActionItemsListViewController: UIViewController {
     
     func reloadTableView(){
         DispatchQueue.main.async {
-            self.tableView.reloadData()
+            UIView.performWithoutAnimation({() -> Void in
+                self.tableView.reloadData()
+                self.tableView!.beginUpdates()
+                self.tableView!.endUpdates()
+            })
         }
     }
     

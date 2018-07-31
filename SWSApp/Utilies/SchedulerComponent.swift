4eb33b1729d80fb7c8a62de3418cc943be58b4b4
shubham.e.shukla <shubham.e.shukla@accenture.com>
2018-07-31 12:06:39 +0530

    Disable all cut copy paste select select all for time textfield.

--- SWSApp/Utilies/SchedulerComponent.swift
+++ SWSApp/Utilies/SchedulerComponent.swift
@@ -204,7 +204,7 @@ class SchedulerComponent: UIView, UITextFieldDelegate, CLLocationManagerDelegate
         }
     }
     override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-        if action ==  "paste:" || action == "copy:"{
+        if action ==  "paste:" || action == "copy:" || action == "cut:" || action == "select:" || action == "selectAll:"{
             return false
         }
         return super.canPerformAction(action, withSender: sender)
